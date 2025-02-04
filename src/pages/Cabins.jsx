0b72ffb373aa7cd2b450ee7968f939aa26a35ceb
alhiefikri <alhiepedia@gmail.com>
2025-02-04 15:39:47 +0800

    finish table cabin and adding some feature filtering and sorting

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -3,6 +3,7 @@ import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
 import { useState } from "react";
 import AddCabin from "../features/cabins/AddCabin";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 
 function Cabins() {
   const [showForm, setShowForm] = useState(false);
@@ -11,7 +12,7 @@ function Cabins() {
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / Sort</p>
+        <CabinTableOperations />
       </Row>
 
       <Row>
