8c9fb2493f28cb25a954b51df1684402d285d041
alhiefikri <alhiepedia@gmail.com>
2025-02-07 18:04:50 +0800

    adding Optional Breakfast and add checking out a Booking + Fixing a Small Bug

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import AppLayout from "./ui/AppLayout";
 import Bookings from "./pages/Bookings";
 import { Toaster } from "react-hot-toast";
 import Booking from "./pages/Booking";
+import Checkin from "./pages/Checkin";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -33,6 +34,7 @@ function App() {
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
             <Route path="bookings/:bookingId" element={<Booking />} />
+            <Route path="checkin/:bookingId" element={<Checkin />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="users" element={<NewUsers />} />
             <Route path="settings" element={<Settings />} />
